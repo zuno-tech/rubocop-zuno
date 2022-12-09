b3b4a385b9aac193102e34ab89db0e9b86013585
Stuart Chinery <stuart.chinery@boxt.co.uk>
2022-12-09 06:31:05 +0000

    lint fix

--- spec/boxt_rubocop_spec.rb
+++ spec/boxt_rubocop_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe BoxtRubocop do
     let(:current_version) { File.read("VERSION").split("\n").first }
 
     it "is set from the VERSION file" do
-      expect(::BoxtRubocop::VERSION).to eq(current_version)
+      expect(BoxtRubocop::VERSION).to eq(current_version)
     end
   end
 end
