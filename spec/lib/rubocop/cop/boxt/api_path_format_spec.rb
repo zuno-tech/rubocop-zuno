7336440692b7022031d91e01dd2ef7c7767df854
Faheem Mughal <faheemmughal@users.noreply.github.com>
2023-09-26 16:25:09 +0100

    [N/A] Move rubocop custom cop out to boxt_rubocop repository (#115)
    * [N/A] Move rubocop custom cop out to boxt_rubocop repository
    
    It was originally implemented here: https://github.com/boxt/boxt-mono/pull/4955 by @jivdhaliwal
    
    * [N/A] Require the file
    
    * [N/A] Fix tests
    
    * [N/A] Update gem file
    
    * [N/A] Added exception to rubocop.
    
    It is just one expectation. The example is more readable by keeping expected result in the example
    
    * [N/A] address comments
    
    * [N/A] Do not increment version. Do not set enable to false
    
    * [N/A] Let's enable ApiPathFormat only for app/api folder
    
    * [N/A] Do not disable the cop at all. It does not seem to effect any of our projects

---
+++ spec/lib/rubocop/cop/boxt/api_path_format_spec.rb
@@ -0,0 +1,89 @@
+# frozen_string_literal: true
+
+require "rubocop/rspec/support"
+
+RSpec.describe RuboCop::Cop::Boxt::ApiPathFormat do
+  include RuboCop::RSpec::ExpectOffense
+
+  subject(:cop) { described_class.new(config) }
+
+  let(:config) { RuboCop::Config.new }
+
+  it "does not register an offense when using get with a path in kebab-case format" do
+    expect_no_offenses(<<~RUBY)
+      class Test < Grape::API
+        get "path/kebab-case"
+      end
+    RUBY
+  end
+
+  it "does not register an offense when using post with a path in kebab-case format" do
+    expect_no_offenses(<<~RUBY)
+      class Test < Grape::API
+        post "path/kebab-case"
+      end
+    RUBY
+  end
+
+  it "does not register an offense when using namespace with a path in kebab-case format" do
+    expect_no_offenses(<<~RUBY)
+      class Test < Grape::API
+        namespace "path/kebab-case"
+      end
+    RUBY
+  end
+
+  it "does not register an offense when using a symbol namespace without any underscores" do
+    expect_no_offenses(<<~RUBY)
+      class Test < Grape::API
+        namespace :path
+      end
+    RUBY
+  end
+
+  # rubocop:disable RSpec/ExampleLength
+  it "registers an offense when using get with a path that contains underscores" do
+    expect_offense(<<~RUBY)
+      class Test < Grape::API
+        get "path/snake_case"
+        ^^^^^^^^^^^^^^^^^^^^^ Boxt/ApiPathFormat: Use kebab-case for the API path
+      end
+    RUBY
+  end
+
+  it "registers an offense when using post with a path that contains underscores" do
+    expect_offense(<<~RUBY)
+      class Test < Grape::API
+        post "path/snake_case"
+        ^^^^^^^^^^^^^^^^^^^^^^ Boxt/ApiPathFormat: Use kebab-case for the API path
+      end
+    RUBY
+  end
+
+  it "registers an offense when using namespace with a path that contains underscores" do
+    expect_offense(<<~RUBY)
+      class Test < Grape::API
+        namespace "path/snake_case"
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^ Boxt/ApiPathFormat: Use kebab-case for the API path
+      end
+    RUBY
+  end
+
+  it "registers an offense when using namespace with a symbol path that contains underscores" do
+    expect_offense(<<~RUBY)
+      class Test < Grape::API
+        namespace :snake_case
+        ^^^^^^^^^^^^^^^^^^^^^ Boxt/ApiPathFormat: Use kebab-case for the API path
+      end
+    RUBY
+  end
+  # rubocop:enable RSpec/ExampleLength
+
+  it "does not register an offense when there is an underscore in the path parameter" do
+    expect_no_offenses(<<~RUBY)
+      class Test < Grape::API
+        get "/test/:snake_case_id/kebab-case"
+      end
+    RUBY
+  end
+end
